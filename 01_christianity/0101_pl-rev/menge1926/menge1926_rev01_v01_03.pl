f8e3bc73755282be60db8437a5e52da9e549765f
blomeen <heiko.blomenkamp@web.de>
2018-12-17 21:31:01 +0100

    New file: menge1926_rev01_v01_03.pl

---
+++ 01_christianity/0101_pl-rev/menge1926/menge1926_rev01_v01_03.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/env perl
+use warnings;
+
+# menge1926_rev01_v01_03.pl
+#
+# Perl: Menge-Bibel 1926; Offenbarung 1, (1) bis (3).
+# Copyright (C) 2018  blomeen <heiko.blomenkamp@web.de>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# Mon Dec 17 20:20:19 UTC 2018
+#
+
+print "\nMenge-Bibel 1926; Offenbarung 1, (1) bis (3).\n\n";
+print "(1) Offenbarung Jesu Christi, die Gott ihm hat zuteil werden lassen, um seinen\n";
+print "Knechten anzuzeigen, was in Bälde geschehen soll; und er hat es durch die\n";
+print "Sendung seines Engels seinem Knechte Johannes durch Zeichen kundgetan,\n";
+print "(2) und dieser legt nunmehr Zeugnis ab von dem Worte Gottes und von dem Zeugnis\n";
+print "Jesu Christi, von allem, was er gesehen hat.\n";
+print "(3) Selig ist der Vorleser und [selig sind] die Hörer der Worte der Weissagung\n";
+print "und die das bewahren, was in ihr geschrieben steht; denn die Zeit [der\n";
+print "Erfüllung] ist nahe.\n";
+print "\n";
+
+
