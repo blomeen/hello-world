7a7596230bc675cb65fcf5c261a86f93be0d3926
blomeen <heiko.blomenkamp@web.de>
2018-12-19 03:19:23 +0100

    New file: mt-f35_rev01_v01_03.pl

---
+++ 01_christianity/0101_pl-rev/mt-f35/mt-f35_rev01_v01_03.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/env perl
+use warnings;
+
+# mt-f35_rev01_v01_03.pl
+#
+# Perl: Byzantine Majority Text F35; Revelation 1, (1) to (3).
+# Copyright (C) 2018  blomeen <heiko.blomenkamp@web.de>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# Date: Wed Dec 19 01:57:22 UTC 2018
+#
+
+print "\nByzantine Majority Text F35; Revelation 1, (1) to (3).\n\n";
+print "(1) αποκαλυψις ιησου χριστου ην εδωκεν αυτω ο θεος δειξαι τοις δουλοις αυτου α\n";
+print "δει γενεσθαι εν ταχει και εσημανεν αποστειλας δια του αγγελου αυτου τω δουλω\n";
+print "αυτου ιωαννη\n";
+print "(2) ος εμαρτυρησεν τον λογον του θεου και την μαρτυριαν ιησου χριστου οσα ειδεν\n";
+print "και ατινα εισιν και α χρη γενεσθαι μετα ταυτα\n";
+print "(3) μακαριος ο αναγινωσκων και οι ακουοντες τους λογους της προφητειας και\n";
+print "τηρουντες τα εν αυτη γεγραμμενα ο γαρ καιρος εγγυς\n";
+print "\n";
+
+
